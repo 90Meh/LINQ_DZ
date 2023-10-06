7b014525db20e762f4c1208cc22be85598a43ec4
meh <meh@IT5>
2023-10-06 16:05:14 +0800

    1

--- LINQ_DZ/Program.cs
+++ LINQ_DZ/Program.cs
@@ -1,23 +1,38 @@
-﻿internal class Program
+﻿using System.Collections.Generic;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        try
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            list.Top(30).ToList().ForEach(x => Console.WriteLine(x));
 
-        Console.WriteLine("Hello, World!");
-        List<int> list = new List<int>();
-        var a = list.Top<int>;
-        Console.ReadKey();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Поймал ошибку");
+            throw;
+        }
     }
 }
 
 public static class IEnumerableExtension
 {
-    public static IEnumerable<T> Top<T> (this IEnumerable<T> values)
+    public static IEnumerable<T> Top<T> (this IEnumerable<T> values, double percent)
     {
-        foreach (var item in values)
-        {    
+        
+        if (!(percent < 1 && percent <= 100))
+        {
             
+            var length = Math.Ceiling(values.Count() / 100 * percent);
+            return values.OrderByDescending(x => x).Take((int)length);
+
         }
-        return values;
+        else
+        {
+            throw new ArgumentException();
+        }          
     }
 }

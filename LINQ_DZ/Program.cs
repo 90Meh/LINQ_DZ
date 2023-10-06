9f4f7ee706b828556683ecb2ac0db3dce04ed083
meh <meh@IT5>
2023-10-06 13:34:24 +0800

    Add project files.

---
+++ LINQ_DZ/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+        List<int> list = new List<int>();
+        var a = list.Top<int>;
+        Console.ReadKey();
+    }
+}
+
+public static class IEnumerableExtension
+{
+    public static IEnumerable<T> Top<T> (this IEnumerable<T> values)
+    {
+        foreach (var item in values)
+        {    
+            
+        }
+        return values;
+    }
+}

a01fdbd3aca4a5702a8d246a7113848226bdf252
meh <meh@IT5>
2023-10-09 14:00:11 +0800

    Final

---
+++ LINQ_DZ/IEnumerableExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_DZ
+{
+    internal static class IEnumerableExtension
+    {
+
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> values, double percent)
+        {
+
+            if (percent < 1 || percent <= 100)
+            {
+                return values.OrderByDescending(x => x).Take((int)Math.Ceiling(values.Count() * percent / 100));
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> values, double percent, Func<T, int> func)
+        {
+            if (percent < 1 || percent <= 100)
+            {             
+                return values.OrderByDescending(func).Take((int)Math.Ceiling(values.Count() * percent / 100));
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
--- LINQ_DZ/Program.cs
+++ LINQ_DZ/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using LINQ_DZ;
+using System.Collections.Generic;
 using System.Linq;
 using static IEnumerableExtension;
 
@@ -9,56 +10,29 @@ internal class Program
         try
         {
             var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            list.Top(30).ToList().ForEach(x => Console.WriteLine(x));
-            var person = new Person("aaa", 32);
-            var listPers = new List<Person>
-            {
-                person
-            };
-            listPers.Top(30, person => person.Age);
+            list.Top(30).ToList().ForEach(x => Console.WriteLine(x));            
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXX");            
+            var persons = new List<Person>() { new Person("Михаил7", 7), new Person("Иван57", 57), new Person("Андрей67", 67), new Person("Пётр23", 23) };
+            persons.Top(60, x => x.Age).ToList().ForEach(x => Console.WriteLine(x.Name));
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            Console.WriteLine("Значение за пределами допустимого.");
+            Console.WriteLine(ex.ToString());
         }
     }
 }
 
 public static class IEnumerableExtension
 {
-    public static IEnumerable<T> Top<T>(this IEnumerable<T> values, double percent)
-    {
-
-        if (percent < 1 || percent <= 100)
-        {
-            return values.OrderByDescending(x => x).Take((int)Math.Ceiling(values.Count() * percent / 100));
-        }
-        else
-        {
-            throw new ArgumentException();
-        }
-    }
-
-    public static IEnumerable<T> Top<T>(this IEnumerable<T> values, double percent, Func<T, bool> predicate)
-    {
-        if (percent < 1 || percent <= 100)
-        {
-            return values.OrderByDescending(x => x).Take((int)Math.Ceiling(values.Count() * percent / 100));
-        }
-        else
-        {
-            throw new ArgumentException();
-        }
-    }
     public class Person
-    {
-        string Name { get; set; }
-        int Age { get; set; }
+    {        
         public Person(string name, int age)
         {
             Name = name;
             Age = age;
         }
+        public string Name { get; set; }
+        public int Age { get; set; }
 
     }
 }
